30168eab5ba986f3d574bc7c1ff3c70f8911264d
Eugene Dobry <edory@gmail.com>
2016-04-16 21:28:37 -0400

    moved code around

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-var nconf = require("nconf"),
-
+var http = require("http"),
+    nconf = require("nconf"),
     register = require("./register");
 
 nconf.argv();
 
-register(nconf.get("path"));
+var LOG = message => console.log(message);
+
+var startServer = port => {
+    LOG(`listening on port ${port}`);
+    http.createServer((req, res) => {
+        LOG("received request");
+
+        var body = "";
+        req.on("data", (chunk) => body += chunk);
+        req.on("end", () => res.end(body));
+    }).listen(port);
+};
+
+register(nconf.get("path"), startServer);
--- register.js
+++ register.js
@@ -1,25 +1,16 @@
-var http = require("http"),
-    request = require("request");
+var request = require("request");
 
 var LOG = message => console.log(message);
 
-var register = path => {
+module.exports = (path, cb) => {
     LOG("registering...");
     request.post({
         url: `http://localhost:1999/${path}`,
         body: "static"
-    }, (err, res, body) => startServer(body));
+    }, (err, res, body) => {
+        if(err)
+            throw new Error(err);
+
+        cb(body);
+    });
 };
-
-var startServer = port => {
-    LOG(`listening on port ${port}`);
-    http.createServer((req, res) => {
-        LOG("received request");
-
-        var body = "";
-        req.on("data", (chunk) => body += chunk);
-        req.on("end", () => res.end(body));
-    }).listen(port);
-};
-
-module.exports = register;
